dc503fe640dec0fd10d31a6086380f5aa53381a0
andudu07 <119854520+andudu07@users.noreply.github.com>
2025-12-22 22:29:40 +0200

    securizare

--- logout.php
+++ logout.php
@@ -1,6 +1,13 @@
 <?php
 require_once 'config.php';
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    die('Method Not Allowed');
+}
+
+require_csrf();
+
 session_unset();
 session_destroy();
 
